2351a30cfc248c68e2d1a98dbeb9f23e7ee3f659
= <=>
2017-11-19 02:34:25 +0100

    Added the log and lil styling

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
 
@@ -13,19 +14,27 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function () {
-            this.monsterHealth -=  this.calculateDamage(3, 10);
-
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -=damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for '+damage,
+            });
             if(this.checkWin()){
                 return;
             }
             this.monsterAttack();
         },
         specialAttack: function () {
-
-            this.monsterHealth -=  this.calculateDamage(10, 20);
-
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -=  damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for '+damage,
+            });
             if(this.checkWin()){
                 return;
             }
@@ -41,6 +50,10 @@ new Vue({
                 this.playerHealth =100
 
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals  for 10'
+            });
             this.monsterAttack();
         },
         giveup: function () {
@@ -51,8 +64,13 @@ new Vue({
             return Math.max(Math.floor(Math.random() * max) +1, min)
         },
         monsterAttack: function(){
-            this.playerHealth -= this.calculateDamage(5,12);
+            var damage =this.calculateDamage(5,12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for '+damage,
+            })
         },
         checkWin: function (){
             if(this.monsterHealth <=0){
